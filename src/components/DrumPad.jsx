6edc12d8a8f2db495533a5881fcf8ac28877d224
Mario Andres Suarez Bravo -X (msuarezb) <msuarezb@cisco.com>
2022-05-03 12:41:00 -0500

    working on useAudio hook

--- src/components/DrumPad.jsx
+++ src/components/DrumPad.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 
-const DrumPad = ( drumTile, play ) => {
+const DrumPad = ( {drumTile, play} ) => {
   console.log(play)
   return (
     <div style={{
         flex: 1
     }}>
-        <button onClick={() => play()} className='drum-pad' id='letterW'>W</button>
-        <button onClick={() => play()} className='drum-pad' id='letterE'>E</button>
-        <button onClick={() => play()} className='drum-pad' id='letterQ'>Q</button>
-        <button onClick={() => play()} className='drum-pad' id='letterA'>A</button>
-        <button onClick={() => play()} className='drum-pad' id='letterS'>S</button>
-        <button onClick={() => play()} className='drum-pad' id='letterD'>D</button>
-        <button onClick={() => play()} className='drum-pad' id='letterZ'>Z</button>
-        <button onClick={() => play()} className='drum-pad' id='letterX'>X</button>
-        <button onClick={() => play()} className='drum-pad' id='letterC'>C</button>
+        <button onClick={() => play.play()} className='drum-pad' id='letterQ'>Q</button>
+        <button className='drum-pad' id='letterE'>E</button>
+        <button className='drum-pad' id='letterW'>W</button>
+        <button className='drum-pad' id='letterA'>A</button>
+        <button className='drum-pad' id='letterS'>S</button>
+        <button className='drum-pad' id='letterD'>D</button>
+        <button className='drum-pad' id='letterZ'>Z</button>
+        <button className='drum-pad' id='letterX'>X</button>
+        <button className='drum-pad' id='letterC'>C</button>
     </div>
   )
 }
--- src/hooks/useAudio.js
+++ src/hooks/useAudio.js
@@ -3,5 +3,4 @@ audio.src = ''
 
 export default function useAudio () {
     console.log('estoy sonando')
-    return audio.play()
 }
